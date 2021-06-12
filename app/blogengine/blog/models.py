4df9b640f73d631cc9e0bda6059e77fe5d7c0fc4
AntonFender <antonfender@mail.ru>
2021-06-13 02:00:59 +0700

    Create BD

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
+class Post(models.Model):
+#Создаем столбцы в нашей БД
+
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+#Когда мы в консоле выводим что-то на печать print там какой-нить объект, то python  возвращает нам объект object
+#и дальше адрес. Это ненаглядно и неудобно. Поэтому обычно классы переопределяют. Метод str который отвечает за
+#вывод информации об объекте. Мы использовали форматирование строк, где {} - место куда будут подставлять данные
+#self.title это заголовок конкретного экземляра класса Post
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 def posts_list(request):
-    n = ['oleg', 'Masha', 'Anton', 'Victor']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
 
+def post_detail(request, slug): # slug мы ждем от сюда path('post/<str:slug>/'...
+
+    post = Post.objects.get(slug__iexact=slug)  #Получаем объект БД по slug
+    return render(request, 'blog/post_detail.html', context={'post': post})
