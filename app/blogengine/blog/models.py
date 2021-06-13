093e04a8b4e719c895ac787f204cf37134901def
AntonFender <antonfender@mail.ru>
2021-06-13 20:12:22 +0700

    Create tags

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
 #Когда мы в консоле выводим что-то на печать print там какой-нить объект, то python  возвращает нам объект object
@@ -20,3 +21,15 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+
+class Tag(models.Model):
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Tag
 
 
 def posts_list(request):
@@ -11,3 +11,11 @@ def post_detail(request, slug): # slug мы ждем от сюда path('post/<s
 
     post = Post.objects.get(slug__iexact=slug)  #Получаем объект БД по slug
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
